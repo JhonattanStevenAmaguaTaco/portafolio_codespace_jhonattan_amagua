5bbaa18c786e44204b06854d79a63291f59693b2
Jhonattan Amagua <86901758+JhonattanStevenAmaguaTaco@users.noreply.github.com>
2023-12-05 15:46:38 +0000

    Site props Info added

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jhonattan Amagua",
+  title: "Web Designer & Software Ingenier student",
+  email: "JhonattanStevenAmagua@outlook.com",
+  gitHub: "JhonattanStevenAmaguaTaco",
+  instagram: "its_jst_a",
+  linkedIn: "JhonattanAmagua",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "JhonattanAmagua",
+  youTube: "AplicanRestricciones",
 };
 
 const primaryColor = "#4E567E";
